6a0c9821b5375baf04bd1e1f40c37e6d078f565b
Yoana Dimova <yoana.dimova@experteer.com>
2018-12-10 14:38:09 +0100

    ADD: Day5a working

--- Day5.js
+++ Day5.js
@@ -1,24 +1,24 @@
 const fetch = require("node-fetch");
 const fs = require('fs');
 
-const filePath = "/Users/yoanadimova/Workspace/adventofcode2018/input5_test.txt";
+const filePath = "/home/yoana/experiments/adventofcode2018/input5.txt";
 const newFile = fs.readFileSync(filePath);
 let data = newFile.toString('utf8');
-console.log(data);
 let res = '';
+let didSth = false;
 
-for (let i=0; i < data.length-1; i++) {
+let i = 0;
+while(i < data.length) {
+		let charCode1 = data.charCodeAt(i);
+		let charCode2 = data.charCodeAt(i+1);
 
-	let charCode1 = data.charCodeAt(i);
-	let charCode2 = data.charCodeAt(i+1);
-
-	if (charCode1 == charCode2 + 32 || charCode1 == charCode2 - 32) {
-		res = data.slice(0, i) + data.slice(i+2, data.length);
-		 //input.slice(0, i).concat(input.slice(i + 2));
-		 //break;
-		console.log(res);
-	}
+		if (charCode1 == charCode2 + 32 || charCode1 == charCode2 - 32) {
+			data = data.slice(0, i) + data.slice(i+2, data.length);
+			i = 0;
+		} else {
+			i++;
+		}
 }
 
-//console.log(res);
+console.log(data.length);
 //dabCBAcaDA
